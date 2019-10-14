d60d15df922fbaea40ccc4e16d479c5dcf7a19e5
Igor7086 <49655092+Igor7086@users.noreply.github.com>
2019-10-14 12:03:58 +0300

    @elpiankova

--- Less4_5_list_3.py
+++ Less4_5_list_3.py
@@ -5,8 +5,6 @@
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use sorted() function and a custom key= function to extract the last element form each tuple.
 
-# sorted(student_tuples, key=lambda student: student[2])
-
 
 def sort_last(tuples):
     return sorted(tuples, key=lambda tup: tup[-1])
